6dc24787e28da50a598f2ea366654d60760f9d90
dongha <donghak937@gmail.com>
2025-06-30 20:22:42 +0900

    add A03

---
+++ A/A03.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int width; // 가로크기
+        int height; // 세로크기
+        int area; // 직사각형의 넓이  
+        System.out.printf("Enter width of Rectangle > ");
+        width = sc.nextInt();
+        sc.nextLine();
+        System.out.printf("Enter height of Rectangle > ");
+        height = sc.nextInt();
+        area = width * height;
+        System.out.printf("Area of Rectangle is %d", area);
+    }
+}
