85c2c1480d9357f8de717fe73b291d6a035d8a1c
dongha <donghak937@gmail.com>
2025-06-30 20:27:43 +0900

    add A05

---
+++ A/A05.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int days; // 날 수
+        int seconds; // 초 단위 시간 
+        System.out.printf("Enter number of days > ");
+        days = sc.nextInt();
+
+        seconds = days * 24 * 60 * 60;
+        System.out.printf("Total seconds is %d", seconds);
+    }
+}
