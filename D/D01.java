60397865bd4d0fdbb89d24458f0dd6d3d405915d
dongha <donghak937@gmail.com>
2025-07-03 19:48:04 +0900

    add D01

---
+++ D/D01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class D01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+        int totalsum = 0;
+        int i;
+
+        System.out.print("Enter a number > ");
+        number = scanner.nextInt();
+
+        if (number <= 1) System.out.println("Invalid input");
+        else {
+            for (i = 1; i <= number; i++) {
+                totalsum += i;
+            }
+            System.out.println("The sum of all integer from 1 to the number entered is " + totalsum);
+        }
+
+    }
+}
