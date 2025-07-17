a1f42ccf5d5ab78cbdcb7fce1f9bf965e4755f37
dongha <donghak937@gmail.com>
2025-07-17 19:49:16 +0900

    add alll

--- N/N01/N01.java
+++ N/N01/N01.java
@@ -11,19 +11,18 @@ class Main {
         Scanner s = new Scanner(System.in);
         HashSet intSet = new HashSet<Integer>();
         int num = Integer.MAX_VALUE;
+        System.out.println("input");
         while (true) {
             num = s.nextInt();
             if (num < 0) break;
             intSet.add(num);
-            //-> 들어간
-
         }
-        //treeset -> 알아서 sorting
-        //sorting
+
         intSet.remove(1);
         List<Integer> list = new ArrayList<>(intSet);
         Collections.sort(list);
 
+        System.out.println("\noutput");
         for (int n : list) {
             System.out.print(n + " ");
         }
